f64eebf69bde7c2a08456c10695edcd9ed8cba58
Dolev Levy <Dolevlevy97@gmail.com>
2024-05-29 14:35:47 +0300

    Add maxDiameter to ball divs and function onBallClick

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 'use strict'
 var ball1Size = 100
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
     ball1Size = ball1Size + getRandomInt(20, 61)
     elBall.style.width = ball1Size + 'px'
     elBall.style.height = ball1Size + 'px'
     elBall.innerHTML = ball1Size
     elBall.style.backgroundColor = getRandomColor()
-    if (ball1Size > 400) {
+    if (ball1Size >= maxDiameter) {
         ball1Size = 100
         elBall.style.width = ball1Size + 'px'
         elBall.style.height = ball1Size + 'px'
