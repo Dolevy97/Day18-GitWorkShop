d77711922458d926b050228b240908ae5046ef44
Dolev Levy <Dolevlevy97@gmail.com>
2024-05-29 14:24:25 +0300

    Add rand color to utils, change ball color

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ function onBallClick(elBall) {
     elBall.style.width = ball1Size + 'px'
     elBall.style.height = ball1Size + 'px'
     elBall.innerHTML = ball1Size
+    elBall.style.backgroundColor = getRandomColor()
     if (ball1Size > 400) {
         ball1Size = 100
         elBall.style.width = ball1Size + 'px'
