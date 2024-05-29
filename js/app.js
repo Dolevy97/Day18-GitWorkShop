c8f416baa061ebf69b06538f30ec674957b57529
Dolev Levy <Dolevlevy97@gmail.com>
2024-05-29 14:15:12 +0300

    Limit ball size JS

--- js/app.js
+++ js/app.js
@@ -6,4 +6,10 @@ function onBallClick(elBall) {
     elBall.style.width = ball1Size + 'px'
     elBall.style.height = ball1Size + 'px'
     elBall.innerHTML = ball1Size
+    if (ball1Size > 400) {
+        ball1Size = 100
+        elBall.style.width = ball1Size + 'px'
+        elBall.style.height = ball1Size + 'px'
+        elBall.innerHTML = ball1Size
+    }
 }
