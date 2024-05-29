3108969fe1250dcb8fef70107bf4627b810874b2
Dolev Levy <Dolevlevy97@gmail.com>
2024-05-29 14:09:53 +0300

    Implement onBallClick

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict'
+var ball1Size = 100
 
 function onBallClick(elBall) {
-    console.log(elBall)
+    ball1Size = ball1Size + 50
+    elBall.style.width = ball1Size + 'px'
+    elBall.style.height = ball1Size + 'px'
+    elBall.innerHTML = ball1Size
 }
